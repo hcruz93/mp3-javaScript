eb37276b6ea43fd529a632fd8ed5b55c30e635a9
hcruz <hum.alex93@gmail.com>
2026-06-15 12:27:03 -0600

    Mapping of all DOM elements that will be manipulated with JavaScript

---
+++ app.js
@@ -0,0 +1,13 @@
+const tituloCancion = document.querySelector(".reproductor-musica h1")
+const nombreArtiste = document.querySelector(".reproductor-musica p")
+
+const progreso = document.getElementById("progreso")
+const cancion = document.getElementById("cancion")
+
+const iconoControl =document.getElementById("iconoControl")
+const botonReproducirPausar = document.querySelector(".controles button.boton-reproducir-pausar");
+
+const botonAtras = document.querySelector(".controles button.atras")
+const botonAdelante = document.querySelector(".controles button.adelante")
+
+// console.log(botonReproducirPausar)
